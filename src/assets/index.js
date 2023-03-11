5235c98354b257dcd4d903bf4d6ac020afcce079
Angel J. Haro <haro.j.angel@gmail.com>
2023-03-11 11:14:33 -0800

    out assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,14 +21,39 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+// ********* WORK Experience *****************
+import apple from "./company/apple.svg";
+import tmobile from "./company/tmobile.png";
+import google from "./company/google.png";
+import kean from "./company/kean.jpeg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+//*********************************************
+
+
+// ********* 3d Balls ************************
+import java from "./tech/java.png";
+import python from "./tech/python.png";
+import linux from "./tech/linux.svg";
+import hacking from "./tech/hacking.svg";
+import gamer from "./tech/gamer.svg";
+import machinelearning from "./tech/machinelearning.svg";
+import cassandra from "./tech/cassandra.png";
+import dataanalyst from "./tech/dataanalyst.svg";
+import datastructures from "./tech/datastructures.svg";
+import cpp from "./tech/cpp.svg";
+import database from "./tech/database.svg";
+
+
+//*********************************************
+import github_projects  from './github_projects.png'
+import vivaldi_projects  from './vivaldi_projects.png'
+import ml_projects  from './ml_projects.png'
+
+
+
 
 export {
   logo,
@@ -52,11 +77,29 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
-};
+
+
+
+  google,
+  apple,
+  java,
+  kean,
+  tmobile,
+  python,
+  linux,
+  hacking,
+  gamer,
+  machinelearning,
+  cassandra,
+  dataanalyst,
+  datastructures,
+  cpp,
+  database,
+
+  github_projects,
+  vivaldi_projects,
+  ml_projects
+}
