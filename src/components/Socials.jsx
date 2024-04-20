dd65331cffaa66d3a945581c47511841fc8a3978
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-19 18:12:49 -0700

    individual component code for socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { useState } from 'react';
-import resume from '/Users/aharo/desk/javascript/portfolio/src/assets/resume[angel-haro].pdf'
-import resume_phone from '/Users/aharo/desk/javascript/portfolio/src/assets/resume[angel-haro].png'
-import { SectionWrapper } from '../hoc';
-import { styles } from '../styles';
-import { fadeIn, textVariant } from '../utils/motion';
-import { isMobile } from 'react-device-detect';
+import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useState } from "react";
+import resume from "/Users/aharo/desk/javascript/portfolio/src/assets/resume[angel-haro].pdf";
+import resume_phone from "/Users/aharo/desk/javascript/portfolio/src/assets/resume[angel-haro].png";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { isMobile } from "react-device-detect";
 
-
-const Socials = () => 
-{
+const Socials = () => {
   const animation = useAnimation();
 
   async function sequence() {
@@ -22,21 +20,21 @@ const Socials = () =>
 
   return (
     <>
-
-
-
-
       <motion.nav>
         {/* Show 'Download Resume' only on non-mobile (desktop) devices */}
         <div className="flex justify-center">
           <motion.div
             style={{
               borderRadius: 100,
-              cursor: 'pointer',
+              cursor: "pointer",
             }}
             onTap={sequence}
             animate={animation}
-            className={!isMobile ? 'flex items-center justify-center bg-white mt-10' : 'hidden'}
+            className={
+              !isMobile
+                ? "flex items-center justify-center bg-white mt-10"
+                : "hidden"
+            }
           >
             <a
               href={resume}
@@ -47,8 +45,6 @@ const Socials = () =>
             </a>
           </motion.div>
         </div>
-
-
 
         {/* Show 'View Resume' only on mobile devices */}
         {/* <div className="flex justify-center">
@@ -79,13 +75,13 @@ const Socials = () =>
           <a
             href={resume}
             className={`${
-              isMobile ? 'flex' : 'hidden'
+              isMobile ? "flex" : "hidden"
             } items-center justify-center px-5 py-2 text-base font-light rounded-full bg-gray-800/90 hover:bg-rose-900/90 focus:outline-none focus:ring-2 focus:ring-offset-2`}
           >
             <motion.div
               style={{
                 borderRadius: 100,
-                cursor: 'pointer',
+                cursor: "pointer",
               }}
               onTap={sequence}
               animate={animation}
@@ -94,79 +90,89 @@ const Socials = () =>
             </motion.div>
           </a>
         </div>
-
-
-
-        
       </motion.nav>
 
-
-
-
-
-
-
-
       <div className="text-center mt-20  rounded-full border-solid border shadow-xl shadow-indigo-500/50">
-          <p className={`${styles.sectionSubText} font-semibold tracking-tighter bg-gradient-to-r from-indigo-500 via-yellow-500 to-pink-500 inline-block 
-          text-transparent bg-clip-text `}>
-              My social medias
-          </p>
+        <p
+          className={`${styles.sectionSubText} font-semibold tracking-tighter bg-gradient-to-r from-indigo-500 via-yellow-500 to-pink-500 inline-block 
+          text-transparent bg-clip-text `}
+        >
+          My social medias
+        </p>
       </div>
 
-
-
-
-
-
-
-      <div className='flex flex-wrap  flex-row flex-1 space-x-0 space-y-3 mx-5 justify-center
+      <div
+        className="flex flex-wrap  flex-row flex-1 space-x-0 space-y-3 mx-5 justify-center
         md:flex-row md:space-x-7 md:justify-center md:flex-1 mt-10
-        '>
-            <a href="https://github.com/aharoJ/"><img 
-              src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" 
-              alt="Github" 
-              className="md:w-full md:h-[42px]  w-full h-[30px] mt-2.5" /></a>&nbsp;
-            <a href="https://discord.gg/HDDQ6pUMHt"><img 
-              src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" 
-              alt="Discord" 
-              className="md:w-full md:h-[40px] w-full/2 h-full"/></a>&nbsp;
-            <a href="https://twitter.com/aharoJ"><img 
-              src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" 
-              alt="Twitter" 
-              className="md:w-full md:h-[40px] w-full/2 h-full"/></a>&nbsp;
-            <a href="https://www.linkedin.com/in/aharoJ/"><img 
-              src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" 
-              alt="Linkedin"
-              className="md:w-full md:h-[40px] w-full/2 h-full" /></a>&nbsp;
-            <a href="https://leetcode.com/aharoJ/"><img 
-            src="https://img.shields.io/badge/-LeetCode-FFA116?style=for-the-badge&logo=LeetCode&logoColor=black" 
+        "
+      >
+        <a href="https://github.com/aharoJ/">
+          <img
+            src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"
+            alt="Github"
+            className="md:w-full md:h-[42px]  w-full h-[30px] mt-2.5"
+          />
+        </a>
+        &nbsp;
+        <a href="https://discord.gg/HDDQ6pUMHt">
+          <img
+            src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white"
+            alt="Discord"
+            className="md:w-full md:h-[40px] w-full/2 h-full"
+          />
+        </a>
+        &nbsp;
+        <a href="https://twitter.com/aharoJ">
+          <img
+            src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white"
+            alt="Twitter"
+            className="md:w-full md:h-[40px] w-full/2 h-full"
+          />
+        </a>
+        &nbsp;
+        <a href="https://www.linkedin.com/in/aharoJ/">
+          <img
+            src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"
+            alt="Linkedin"
+            className="md:w-full md:h-[40px] w-full/2 h-full"
+          />
+        </a>
+        &nbsp;
+        <a href="https://leetcode.com/aharoJ/">
+          <img
+            src="https://img.shields.io/badge/-LeetCode-FFA116?style=for-the-badge&logo=LeetCode&logoColor=black"
             alt="LeetCode"
-            className="md:w-full md:h-[40px] w-full/2 h-full" /></a>&nbsp;
-            <a href="mailto: haro.j.angel@gmail.com"><img 
-            src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" 
+            className="md:w-full md:h-[40px] w-full/2 h-full"
+          />
+        </a>
+        &nbsp;
+        <a href="mailto: haro.j.angel@gmail.com">
+          <img
+            src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"
             alt="Gmail"
-            className="md:w-full md:h-[40px] w-full/2 h-full" /></a>&nbsp;
+            className="md:w-full md:h-[40px] w-full/2 h-full"
+          />
+        </a>
+        &nbsp;
       </div>
-
-
-
-
 
       <motion.p
-        variants={fadeIn("","",0.1,7.1)}
-        className='mt-10 text-[20px] max-w-3xl leading-[20px]
+        variants={fadeIn("", "", 0.1, 7.1)}
+        className="mt-10 text-[20px] max-w-3xl leading-[20px]
         bg-gradient-to-r from-indigo-500 via-yellow-500 to-pink-500 inline-block text-transparent bg-clip-text 
-        text-center mx-0 md:mx-48 font-light tracking-wide '
+        text-center mx-0 md:mx-48 font-light tracking-wide "
       >
-        As a driven and ambitious computer scientist, I am eager to make a lasting impact in the world. Despite facing challenges early in my career due to my humble upbringing, I have revived my enthusiasm for technology and am focused on achieving recognition in the field. With a passion for software architecture, machine learning, pipelines, and hacking, I am committed to lifelong learning and embody Albert Einstein's words: "Wisdom is not a product of schooling but of the lifelong attempt to acquire it."
+        As a driven and ambitious computer scientist, I am eager to make a
+        lasting impact in the world. Despite facing challenges early in my
+        career due to my humble upbringing, I have revived my enthusiasm for
+        technology and am focused on achieving recognition in the field. With a
+        passion for software architecture, machine learning, pipelines, and
+        hacking, I am committed to lifelong learning and embody Albert
+        Einstein's words: "Wisdom is not a product of schooling but of the
+        lifelong attempt to acquire it."
       </motion.p>
-
-
-
-
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Socials, 'socials');
+export default SectionWrapper(Socials, "socials");
